489f457fe266fc527d39486ecb0e6c8fd2ea4beb
simplyyrayyan <simplyyrayyan@gmail.com>
2026-04-26 15:28:08 -0400

    Implement dictionary words check for passwords
    Added a function to check if generated passwords contain dictionary words, enhancing security validation.

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
-# Password Generator    
+# Password Generator
+# MESSAGE: Add dictionary words check function into Program
 
 # Imports
 import string
 import time
 import sys
 import random
+import secrets 
+import pyperclip
+
+# Opening the Dictionary
+try:
+    with open(r"C:\Users\simpl\OneDrive\Documents\DEVELOPER\Learning Projects\Python Learning Projects\words.txt", "r") as f:
+        WORD_LIST = [line.strip().lower() for line in f]
+except FileNotFoundError:
+    print("The dictionary file was not found.")
+    sys.exit()
 
 # Functions
-
 # Asking to Continue Function 
-
 def ask_to_continue():
     user_input1 = input("Would you like to (Quit) or (Continue)?: ").lower().strip()
     if user_input1 == "quit":
@@ -25,37 +34,23 @@ def ask_to_continue():
     else:
         print("Invalid Input, Please Try Again.")
         return True
-# Guarentee One Option Function 
-def warning_for_no_nl(option, message, message1):
-    if option == "no":
-            time.sleep(0.5)
-            print("Not using " + message1 + " in password is not Recommended as it can make your password less secure")
-            time.sleep(0.5)
-            ask = str(input(message)).lower().strip()
-            if ask in ["yes", "y", "yeah", "yep", "sure", "YESSS", "yesssss", "yea", "ye", "yesss"]:
-                pass
+# Checking Password if it contains Dictionary Words
+def check_password(password):
+    password_lower = password.lower()
+    return any(word in password_lower for word in WORD_LIST)
 
-            if ask in ["no", "n", "nope", "nah", "NOOOO", "noooooo", "naw", "na", "noooo"]:
-                time.sleep(0.5)
-                print("Okay, Restarting Program.....")
-                time.sleep(0.5)
-                return False
-                
-            else:
-                time.sleep(0.5)
-                print("Invalid Input, Exiting Program.....")
-                time.sleep(0.5)
-                sys.exit()
-
-
+    
 # Variables
-message_l = "Would you like to use letters in your password? (Yes/No): "
-message_n = "Would you like to use numbers in your password? (Yes/No): "
-message_s = "Would you like to use symbols in your password? (Yes/No): "
-
 message1_l = "Are you sure you want to continue without using letters? (Yes/No): "
 message1_n = "Are you sure you want to continue without using numbers? (Yes/No): "
 message1_s = "Are you sure you want to continue without using symbols? (Yes/No): "
+
+password_message1 = "Your Password is Strong!"
+password_message2 = "Your Password is Secure!"
+password_message3 = "Your Password is Okay!"
+password_message4 = "Your Password is Weak!"
+password_message5 = "Your Password is Very Weak! Do not use this password!"
+
 
 # Greeting
 time.sleep(1)
@@ -65,7 +60,7 @@ while True:
     # User Input and Validation
     try:
         time.sleep(0.5)
-        user_choice = int(input("How long would you like your password to be?: "))
+        user_choice = int(input("How long would you like your password to be? \n (Password Might be longer than this) \n (7-99): "))
         if user_choice >= 100 or user_choice <= 6:
             print("Password must be less than 100 characters long. \nPassword must be more than 6 characters long")
             time.sleep(0.5)
@@ -78,65 +73,34 @@ while True:
         time.sleep(0.5)
         continue
 
-    try: 
+    try:
+
         characters = ""
 
-        # Ask If they want to use letters
-        letters = str(input(message_l)).lower().strip()
-       
-        # Ask If they want to use numbers
-        numbers = str(input(message_n)).lower().strip()
-
-        # Ask If they want to use symbols
-        symbols = str(input(message_s)).lower().strip()
-
-        # Normalize User Input
+        preset = input("Choose one, \n 1. Easy (A-Z) \n 2. Medium (A-Z, 0-9) \n 3. Hard (A-Z, 0-9, Symbols): ").lower().strip()
 
         # This allows the user to input different variations of yes and will make it still work
-        if letters in ["yes", "y", "yeah", "yep", "sure", "YESSS", "yesssss", "yea", "ye", "yesss"]:
-            letters = "yes"
-        if numbers in ["yes", "y", "yeah", "yep", "sure", "YESSS", "yesssss", "yea", "ye", "yesss"]:
-            numbers = "yes"
-        if symbols in ["yes", "y", "yeah", "yep", "sure", "YESSS", "yesssss", "yea", "ye", "yesss"]:
-            symbols = "yes"
-        # This allows the user to input different variations of no and will make it still work
-        if letters in ["no", "n", "nope", "nah", "NOOOO", "noooooo", "naw", "na", "noooo"]:
-            letters = "no"
-        if numbers in ["no", "n", "nope", "nah", "NOOOO", "noooooo", "naw", "na", "noooo"]:
-            numbers = "no"    
-        if symbols in ["no", "n", "nope", "nah", "NOOOO", "noooooo", "naw", "na", "noooo"]:
-            symbols = "no"
+        if preset in ["1", "Easy", "easy", "EASY", "1. Easy", "1. easy", "1. EASY"]:
+            preset = "easy"
+        elif preset in ["2", "Medium", "medium", "MEDIUM", "2. Medium", "2. medium", "2. MEDIUM"]:
+            preset = "medium"
+        elif preset in ["3", "Hard", "hard", "HARD", "3. Hard", "3. hard", "3. HARD"]:
+            preset = "hard"
+        
         
         # Selecting Character Pool
-        if letters == "yes":
+        if preset == "easy":
             characters += string.ascii_letters
-        if numbers == "yes":
-            characters += string.digits
-        if symbols == "yes":
-            characters += string.punctuation
+        elif preset == "medium":
+            characters += string.ascii_letters + string.digits
+        elif preset == "hard":
+            characters += string.ascii_letters + string.digits + string.punctuation
         
         # Validation
 
         # If They user puts in an invalid input for any of the options it will ask them to try again and restart the program
-        if letters not in ["yes", "no"] or numbers not in ["yes", "no"] or symbols not in ["yes", "no"]:
+        if preset not in ["easy", "medium", "hard"]:
             print("Invalid Input, Please Try Again")
-            time.sleep(0.5)
-            continue
-
-        # If User Puts in no for letters
-        if warning_for_no_nl(letters, message1_l, "letters") == False:
-            continue
-        # If User Puts in no for numbers
-        if warning_for_no_nl(numbers, message1_n, "numbers") == False:
-            continue
-        # If User Puts in no for symbols
-        if warning_for_no_nl(symbols, message1_s, "symbols") == False:
-            continue
-        
-        # If the user chooses no for all options it will ask them to choose at least one option and restart the program
-        if letters == "no" and numbers == "no" and symbols == "no":
-            print("You have to choose at least one of the options to generate a password")
-            print("Restarting Program.....")
             time.sleep(0.5)
             continue
 
@@ -147,11 +111,150 @@ while True:
         time.sleep(0.5)
         continue 
 
+    # Generating and Printing the Password(s)
+    time.sleep(0.4)
+    try:
+        count = int(input("How many passwords would you like to generate?: "))
+        if count == 0:
+            print("You Can not generate 0 passwords")
+            time.sleep(0.4)
+            continue
+        elif count < 0:
+            print("You Can not generate a negative amount of passwords")
+            time.sleep(0.4)
+            continue
+        elif count > 100:
+            print("You Can not generate more than 100 passwords")
+            time.sleep(0.4)
+            continue
+        
+    except ValueError:
+        print("Error Restarting Program.....")
+        time.sleep(0.5)
+        continue
+    
+    all_passwords = []
 
-    # Generating the Password
-    password = "".join(random.choice(characters) for _ in range(user_choice))
-    print(f" Your Password is: {password}")
-    print(f"The Length of your password is: {len(password)}")
-    time.sleep(1)
+    for i in range(count):
+        password = "".join(secrets.choice(characters) for _ in range(user_choice))
+
+        # Making sure The password has atleast one of each character type
+
+        # Easy -> A-Z
+        if preset == "easy":
+            if not any(c.isalpha() for c in password):
+                password += secrets.choice(string.ascii_letters)
+        
+        # Medium -> A-Z, 0-9
+        elif preset == "medium":
+            if not any(c.isalpha() for c in password):
+                password += secrets.choice(string.ascii_letters)
+            if not any(c.isdigit() for c in password):
+                password += secrets.choice(string.digits)
+
+        # Hard -> A-Z, 0-9, Symbols
+        elif preset == "hard":
+            if not any(c.isalpha() for c in password):
+                password += secrets.choice(string.ascii_letters)
+            if not any(c.isdigit() for c in password):
+                password += secrets.choice(string.digits)
+            if not any(not (c.isalnum() or c.isspace()) for c in password):
+                password += secrets.choice(string.punctuation)
+
+        # Shuffling the Password
+        password = "".join(random.SystemRandom().sample(password, len(password)))
+    
+        # Checking if password is Weak/Okay/Strong
+        password_strength = 0
+
+        letters1 = sum(c.isalpha() for c in password)
+        numbers1 = sum(c.isdigit() for c in password)
+        symbols1 = sum(not (c.isalnum() or c.isspace()) for c in password)
+        length = len(password)
+
+        if length >= 8:
+            password_strength += 1
+        if letters1 >= 1:
+            password_strength += 1
+        if numbers1 >= 1:
+            password_strength += 1
+        if symbols1 >= 1:
+            password_strength += 1
+        if length >= 12:
+            password_strength += 1
+    
+        time.sleep(0.3)
+        hidden_password = str(input("***********, Your Password is Hidden Type (REVEAL) to see: ")).lower().strip()
+        if hidden_password in ["reveal", "REVEAL", "Reveal", "REVEAL", "REAVEAL", "reaveal"]:
+            print(f" {i + 1}. Password: {password} \n Length: {len(password)} \n Rating: {password_strength}/5")
+        else:
+            print("You Typed the wrong command")
+            time.sleep(0.3)
+            print("********")
+            ask_to_continue()
+
+        # Printing and Checking Password Strength 
+        if password_strength >= 5:
+            time.sleep(0.5)
+            print(password_message1)# Strong Password
+        elif password_strength >= 4:
+            time.sleep(0.5)
+            print(password_message2) # Secure Password
+        elif password_strength >= 3:
+            time.sleep(0.5)
+            print(password_message3) # Okay Password
+        elif password_strength >= 2:
+            time.sleep(0.5)
+            print(password_message4) # Weak Password
+        elif password_strength >= 1:
+            time.sleep(0.5)
+            print(password_message5) # Very Weak Password
+            time.sleep(0.5)
+        all_passwords.append(password)
+
+    # Asking if they wanted to Save the password(s) to a text file
+    try: 
+        ask_to_save = input("Would you like to save the password(s) to a text file? (Yes/No): ").lower().strip()
+        if ask_to_save == "yes":
+            time.sleep(0.5)
+            with open("password.txt", "a") as file:
+                file.write("Generated Passwords:\n")
+                for i, p in enumerate(all_passwords, start=1):
+                    file.write(f"{i}. {p}\n")
+                file.write("\n")
+            print("Password(s) saved to password.txt")
+        elif ask_to_save == "no":
+            time.sleep(0.5)
+            print("Password(s) not saved to password.txt")
+        else:
+            time.sleep(0.5)
+            print("Invalid Input, Password(s) not saved to password.txt")
+    except ValueError:
+        print("Error Restarting Program.....")
+        time.sleep(0.5)
+        continue
+
+    # Asking if they wanted to copy to clipboard
+
+    try: 
+        ask_to_copy = input("Would you like to copy the password(s) to the clipboard? (Yes/No): ").lower().strip()
+        if ask_to_copy == "yes":
+            time.sleep(0.5)
+            pyperclip.copy("\n".join(all_passwords))
+            print("Password(s) copied to clipboard")
+        elif ask_to_copy == "no":
+            time.sleep(0.5)
+            print("Password(s) not copied to clipboard")
+        else:
+            time.sleep(0.5)
+            print("Invalid Input, Password(s) not copied to clipboard")
+    except Exception as e:
+        print(f"Clipboard Error: {e}")
+        time.sleep(0.5)
+        print("Restarting Program.....")
+        time.sleep(0.5)
+        continue
+
     ask_to_continue()
-# Code Ends
+
+# Code Ends 
