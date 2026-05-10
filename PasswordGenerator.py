80a0b91f64863f13d99d10ef050e3b7f4c903194
simplyyrayyan <simplyyrayyan@gmail.com>
2026-05-10 09:44:30 -0400

    Implement slow printing and input in PasswordGenerator
    Added slow printing and slow input features for user interactions.

--- PasswordGenerator.py
+++ PasswordGenerator.py
@@ -1,15 +1,16 @@
 # Password Generator Program
 
-# MESSAGE: Fix when the password is longer then what user requires.
+# MESSAGE: add slow printing and slow input
 
 # Imports
 import secrets, time, sys, string, pyperclip 
+from slow_printing import slow_print, slow_input
 # Opening the Dictionary
 try:
     with open(r"C:\Users\simpl\OneDrive\Documents\DEVELOPER\Learning Projects\Python Learning Projects\words.txt", "r") as f:
         WORD_LIST = set([line.strip().lower() for line in f])
 except FileNotFoundError:
-    print("The dictionary file was not found.")
+    slow_print("The dictionary file was not found.")
     sys.exit()
 
 # Functions
@@ -17,23 +18,19 @@ except FileNotFoundError:
 # Asking the user if they want to continue.
 def ask_to_continue():
     try:
-        user_input1 = input("Would you like to (Quit) or (Continue)?: ").lower().strip()
+        user_input1 = slow_input("Would you like to (Quit) or (Continue)?: ").lower().strip()
         if user_input1 == "quit":
-            time.sleep(0.8)
-            print("Thanks for using the Password Generator hope to see you again!")
+            slow_print("Thanks for using the Password Generator hope to see you again!")
             sys.exit()
         elif user_input1 == "continue":
-            time.sleep(0.8)
             return True
         elif user_input1 == "":
-            time.sleep(0.8)
             return True
         else:
-            print("Invalid Input, Please Try Again.")
+            slow_print("Invalid Input, Please Try Again.")
             return True
     except ValueError:
-        print("Error Exiting Program.....")
-        time.sleep(0.5)
+        slow_print("Error Exiting Program.....")
         sys.exit()
 # Checking Password if it contains Dictionary Words
 def contains_dictionary_word(password: str): 
@@ -42,77 +39,66 @@ def contains_dictionary_word(password: str):
         password_lower = password.lower()
         return any(word in password_lower for word in WORD_LIST)
     except ValueError:
-        print("Error Exiting Program.....")
-        time.sleep(0.5)
+        slow_print("Error Exiting Program.....")
         sys.exit()
 # Saving Password to a Text File Function
 def save_txt_file():
     try: 
-        ask_to_save = input("Would you like to save the password(s) to a text file? (Yes/No): ").lower().strip()
-        if ask_to_save == "yes":
-            time.sleep(0.5)
+        ask_to_save = slow_input("Would you like to save the password(s) to a text file? (Yes/No): ").lower().strip()
+        if ask_to_save in ["yes", "yea", "y", "ye"]:
             with open("password.txt", "a") as file:
                 file.write("Generated Passwords:\n")
                 for i, p in enumerate(all_passwords, start=1):
                     file.write(f"{i}. {p}\n")
                 file.write("\n")
-            print("Password(s) saved to password.txt")
-        elif ask_to_save == "no":
-            time.sleep(0.5)
-            print("Password(s) not saved to password.txt")
+            slow_print("Password(s) saved to password.txt")
+        elif ask_to_save in ["no", "n", "no"]:
+            slow_print("Password(s) not saved to password.txt")
         else:
-            time.sleep(0.5)
-            print("Invalid Input, Password(s) not saved to password.txt")
+            slow_print("Invalid Input, Password(s) not saved to password.txt")
     except ValueError:
-        print("Error Exiting Program.....")
-        time.sleep(0.5)
+        slow_print("Error Exiting Program.....")
         sys.exit()
 # Copy to Clipboard Function
 def copy_to_clipboard():
     try: 
-        ask_to_copy = input("Would you like to copy the password(s) to the clipboard? (Yes/No): ").lower().strip()
+        ask_to_copy = slow_input("Would you like to copy the password(s) to the clipboard? (Yes/No): ").lower().strip()
         if ask_to_copy in ["yes", "yea", "y", "ye"]:
-            time.sleep(0.5)
             pyperclip.copy("\n".join(all_passwords))
-            print("Password(s) copied to clipboard")
+            slow_print("Password(s) copied to clipboard")
         elif ask_to_copy in ["no", "n", "no"]:
-            time.sleep(0.5)
-            print("Password(s) not copied to clipboard")
+            slow_print("Password(s) not copied to clipboard")
         else:
-            time.sleep(0.5)
-            print("Invalid Input, Password(s) not copied to clipboard")
+            slow_print("Invalid Input, Password(s) not copied to clipboard")
     except Exception as e:
-        print(f"Clipboard ERROR: {e} ")
-        time.sleep(0.5)
-        print("Exiting Program.....")
-        time.sleep(0.5)
+        slow_print(f"Clipboard ERROR: {e} ")
+        slow_print("Exiting Program.....")
         sys.exit()
 # Password Strength Check Function
 def password_strength_check():
 # Password Strength Check 
     if password_strength >= 5:
-        print(password_message1) # Strong Password
+        slow_print(password_message1) # Strong Password
 
     elif password_strength >= 4:
-        print(password_message2) # Secure Password
+        slow_print(password_message2) # Secure Password
 
     elif password_strength >= 3:
-        print(password_message3) # Okay Password
+        slow_print(password_message3) # Okay Password
 
     elif password_strength >= 2:
-        print(password_message4) # Weak Password
+        slow_print(password_message4) # Weak Password
 
     elif password_strength >= 1:
-        print(password_message5) # Very Weak Password
+        slow_print(password_message5) # Very Weak Password
 # Password Reveal Function and Printing Password
 def password_reveal():
-    hidden_password = str(input("***********, Your Password is Hidden Type (REVEAL) to see: ")).lower().strip()
+    hidden_password = str(slow_input("***********, Your Password is Hidden Type (REVEAL) to see: ")).lower().strip()
     if hidden_password in ["reveal", "REVEAL", "Reveal", "REVEAL", "REAVEAL", "reaveal"]:
-        print(f" {i + 1}. Password: {password} \n Length: {len(password)} \n Rating: {password_strength}/5")
+        slow_print(f" {i + 1}. Password: {password} \n Length: {len(password)} \n Rating: {password_strength}/5", speed=(0.05) )
     else:
-        print("You Typed the wrong command")
-        time.sleep(0.3)
-        print("********")
+        slow_print("You Typed the wrong command")
+        slow_print("********")
         ask_to_continue()
 # If there is a Repeated Character
 def is_repeated_chars(password: str):
@@ -169,10 +155,12 @@ def is_weak_password(password: str):
         return True
     if is_repeated_chars(password) == True:
         return True
-    elif is_sequential_chars(password) == True:
+    if is_sequential_chars(password) == True:
         return True
-    elif is_keyboard_pattern(password) == True:
+    if is_keyboard_pattern(password) == True:
         return True
+    
+    return False
     
     
 # Variables
@@ -186,7 +174,7 @@ password_message3 = "Your Password is Okay!"
 password_message4 = "Your Password is Weak!"
 password_message5 = "Your Password is Very Weak! Do not use this password!"
 
-password_length_question = "How long would you like your password to be? \n (Password Might be longer than this) \n (7-99): "
+password_length_question = "How long would you like your password to be?\n (7-99): "
 error_message1 = "Password must be less than 100 characters long. \nPassword must be more than 6 characters long"
 greeting = "Hello and Welcome to the Password Generator"
 restart_message = "Restarting Program....."
@@ -194,36 +182,33 @@ restart_message = "Restarting Program....."
 easy_list = ["1", "Easy", "easy", "EASY", "1. Easy", "1. easy", "1. EASY", "esy", "ESY", "esY", "EsY"]
 medium_list = ["2", "Medium", "medium", "MEDIUM", "2. Medium", "2. medium", "2. MEDIUM", "Medum", "MEDUM", "medum"]
 hard_list = ["3", "Hard", "hard", "HARD", "3. Hard", "3. hard", "3. HARD"]
-all_passwords = []
 
 try:
     # Greeting
-    time.sleep(1)
-    print(greeting)
+    slow_print(greeting)
 
     # Main Loop & Program
     while True:
+
+        # Defining all_passwords
+        all_passwords = []
         # User Input and Validation
         try:
-            time.sleep(0.5)
-            password_length = int(input(password_length_question))
+            password_length = int(slow_input(password_length_question))
             if password_length >= 100 or password_length <= 6:
-                print(error_message1)
-                time.sleep(0.5)
+                slow_print(error_message1)
                 continue
 
         except Exception as error:
-            print(f"You have error {error}")
-            time.sleep(0.5)
-            print(restart_message)
-            time.sleep(0.5)
+            slow_print(f"You have error {error}")
+            slow_print(restart_message)
             continue
 
         try:
 
             characters = ""
 
-            preset = input("Choose one, \n 1. Easy (A-Z) \n 2. Medium (A-Z, 0-9) \n 3. Hard (A-Z, 0-9, Symbols): ").lower().strip()
+            preset = slow_input("Choose one, \n 1. Easy (A-Z) \n 2. Medium (A-Z, 0-9) \n 3. Hard (A-Z, 0-9, Symbols): ").lower().strip()
 
             # This allows the user to input different variations of yes and will make it still work
             if preset in easy_list:
@@ -246,44 +231,37 @@ try:
 
             # If They user puts in an invalid input for any of the options it will ask them to try again and restart the program
             if preset not in ["easy", "medium", "hard"]:
-                print("Invalid Input, Please Try Again")
-                time.sleep(0.5)
+                slow_print("Invalid Input, Please Try Again")
                 continue
 
         except Exception as error1:
-            print(f"You have error {error1}")
-            time.sleep(0.5)
-            print(restart_message)
-            time.sleep(0.5)
+            slow_print(f"You have error {error1}")
+            slow_print(restart_message)
             continue 
 
         # Get and limit password count to a valid range (1-100)
-        time.sleep(0.4)
         try:
-            count = int(input("How many passwords would you like to generate?: "))
+            count = int(slow_input("How many passwords would you like to generate?: "))
             if count == 0:
-                print("You Can not generate 0 passwords")
-                time.sleep(0.4)
+                slow_print("You Can not generate 0 passwords")
                 continue
             elif count < 0:
-                print("You Can not generate a negative amount of passwords")
-                time.sleep(0.4)
+                slow_print("You Can not generate a negative amount of passwords")
                 continue
             elif count > 100:
-                print("You Can not generate more than 100 passwords")
-                time.sleep(0.4)
+                slow_print("You Can not generate more than 100 passwords")
                 continue
 
         
         except ValueError:
-            print(restart_message)
-            time.sleep(0.5)
+            slow_print(restart_message)
             continue
     
 
     # Generating the Password
         for i in range(count):
             retry_counter = 0
+            password_strength = 0
 
             while True:
 
@@ -305,19 +283,19 @@ try:
 
                 password += "".join(secrets.choice(characters) for _ in range(remaining_length))
 
+                # Shuffling the Password
+                password = "".join(secrets.SystemRandom().sample(password, len(password)))  
+
                 # Seeing if the password has repeated characters
                 if is_weak_password(password):
                     retry_counter += 1
-                    if retry_counter >= 10:
+                    if retry_counter >= 100:
                         break
                     else:
                         continue
-       
-            # Shuffling the Password
-            password = "".join(secrets.SystemRandom().sample(password, len(password)))  
+                break
 
             # Checking if password has atleast one of each character type
-            password_strength = 0
 
             letters_check = sum(c.isalpha() for c in password)
             numbers_check = sum(c.isdigit() for c in password)
@@ -334,9 +312,6 @@ try:
                 password_strength += 1
             if length_check >= 14:
                 password_strength += 1
-        
-            if length_check > password_length:
-                continue
         
             # Adding the Passwords to all_paswords list
             all_passwords.append(password)
@@ -358,8 +333,7 @@ try:
 
 # If user presses Ctrl + C/c then program will exit anywhere
 except KeyboardInterrupt:
-    print("Thanks for using Password Generator")
-    time.sleep(0.5)
+    slow_print("Thanks for using Password Generator")
     sys.exit()
 
 # Code Ends Here
